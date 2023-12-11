cb8f52b17700bc755aebda432281fb24bfff24c0
Tabaro Christian <tabarochristian@yahoo.com>
2023-12-11 13:49:21 +0800

    auth login

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
+from rest_framework.authtoken import views
 from django.urls import path
 from .views import *
 
 app_name = 'api'
 
 urlpatterns = [
+    path('auth/token', views.obtain_auth_token),
     path('list/<str:app>/<str:model>', List.as_view(), name='list'),
     path('create/<str:app>/<str:model>', Create.as_view(), name='create'),
     path('detail/<str:app>/<str:model>/<int:pk>', Detail.as_view(), name='detail'),
